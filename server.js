1527065a4897aba78047d7cc056ad8bfd1586796
Sascha Majewsky <sascha.majewsky@pm.me>
2020-12-21 06:30:36 +0100

    Added register route

--- server.js
+++ server.js
@@ -9,10 +9,27 @@ const server = express();
 // true argument will make push command also save to db
 const db = new JsonDB(new Config("totp", true, false, "/"));
 
-server.get("/api", (req, res) =>
+server.get("/v1/", (req, res) =>
   res.json({
     message: "Welcome to the time-based one time password (TOTP) API",
   })
 );
+
+// Register user and create secret
+
+server.get("/v1/users/register", (req, res) => {
+  const id = uuid.v4();
+
+  // speakeasy provides ascii, hex, base32 and otpauth_url (QR-code for frontned) but we only return base32
+  try {
+    const path = `/user/${id}`;
+    const temp_secret = speakeasy.generateSecret();
+    db.push(path, { id, temp_secret });
+    res.json({ id, secret: temp_secret.base32 });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Interal eror during register." });
+  }
+});
 
 module.exports = server;
