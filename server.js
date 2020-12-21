be6c75f47008f03a0e4aad05ef946c7421ce8cca
Sascha Majewsky <sascha.majewsky@pm.me>
2020-12-21 06:10:16 +0100

    Added and initialized node-json-db

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const speakeasy = require("speakeasy");
+const uuid = require("uuid");
+const { JsonDB } = require("node-json-db");
+const { Config } = require("node-json-db/dist/lib/JsonDBConfig");
 
 const server = express();
+
+// true argument will make push command also save to db
+const db = new JsonDB(new Config("totp", true, false, "/"));
 
 server.get("/api", (req, res) =>
   res.json({
